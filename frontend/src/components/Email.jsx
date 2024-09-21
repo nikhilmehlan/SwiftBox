aa806a9b976e723e47db6df281fbd5038ca5332b
nikhilmehlan <145481279+nikhilmehlan@users.noreply.github.com>
2024-09-21 21:56:06 +0530

    email and emails included

---
+++ frontend/src/components/Email.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { MdCropSquare } from "react-icons/md";
+import { RiStarLine } from "react-icons/ri";
+const Email = () => {
+  return (
+    <div className="flex items-center justify-between border-b border-gray-200 px-4 py-3 text-sm hover: cursor-pointer hover:shadow-md">
+      <div className="flex gap-3 items-center">
+        <div className="text-gray-400">
+          <MdCropSquare size={"20px"} />
+        </div>
+        <div className="text-gray-400">
+          <RiStarLine size={"20px"} />
+        </div>
+        <div>
+            <h1 className="font-semibold">Nikhil Mehlan</h1>
+        </div>
+      </div>
+      <div className="flex-1 ml-4">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum unde
+          fuga praesentium nulla quis eligendi.
+        </p>
+      </div>
+      <div className="flex-none text-gray text-sm">
+        <p>12 days ago</p>
+      </div>
+    </div>
+  );
+};
+
+export default Email;
--- frontend/src/components/InboxM.jsx
+++ frontend/src/components/InboxM.jsx
@@ -1,11 +1,34 @@
-import React from "react";
-import { MdCropSquare, MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+import React, { useState } from "react";
+import {
+  MdCropSquare,
+  MdInbox,
+  MdKeyboardArrowLeft,
+  MdKeyboardArrowRight,
+} from "react-icons/md";
 import { IoMdArrowDropdown, IoMdMore, IoMdRefresh } from "react-icons/io";
+import { GoTag } from "react-icons/go";
+import { FaUserFriends } from "react-icons/fa";
+import Emails from "./Emails";
 
+const mailType = [
+  {
+    icon: <MdInbox size={"20px"} />,
+    text: "Primary",
+  },
+  {
+    icon: <GoTag size={"20px"} />,
+    text: "Promotions",
+  },
+  {
+    icon: <FaUserFriends size={"20px"} />,
+    text: "Socials",
+  },
+];
 const InboxM = () => {
+  const [selected, setSelected] = useState(0);
   return (
     <div className="flex-1 bg-white rounded-xl mx-5">
-      <div className="flex items-center justify-between px-4">
+      <div className="flex items-center justify-between px-4 my-2">
         <div className="flex items-center gap-2">
           <div className="flex items-center gap-1">
             <MdCropSquare size={"20px"} />
@@ -20,9 +43,36 @@ const InboxM = () => {
         </div>
         <div className="flex items-center gap-2">
           <span>1 - 50 </span>
-          <MdKeyboardArrowLeft size={'24px'} className="rounded-full hover:bg-gray-200 cursor-pointer"/>
-          <MdKeyboardArrowRight size={'24px'} className="rounded-full hover:bg-gray-200 cursor-pointer"/>
-
+          <MdKeyboardArrowLeft
+            size={"24px"}
+            className="rounded-full hover:bg-gray-200 cursor-pointer"
+          />
+          <MdKeyboardArrowRight
+            size={"24px"}
+            className="rounded-full hover:bg-gray-200 cursor-pointer"
+          />
+        </div>
+      </div>
+      <div className="h-40vh overflow-y-auto">
+        <div className="flex items-center gap-1">
+          {mailType.map((item, index) => {
+            return (
+              <button
+                onClick={() => setSelected(index)}
+                className={` ${
+                  selected == index
+                    ? "border-b-4 border-b-blue-600 text-blue-600"
+                    : "border-b-4 border-b-transparent-600 text-transparent-600"
+                } flex items-center gap-5 p-4 w-52 hover:bg-gray-200`}
+              >
+                {item.icon}
+                <span>{item.text}</span>
+              </button>
+            );
+          })}
+        </div>
+        <div>
+          <Emails/>
         </div>
       </div>
     </div>
